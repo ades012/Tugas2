2beac105b3a599859762cf5f811dc0f08a31e7d8
ades012 <gngsptn@gmail.com>
2017-01-20 20:47:45 +0700

    perubahan1

---
+++ HitungLuas.java
@@ -0,0 +1,11 @@
+class HitungLuas {
+        public int cariLuasPersegiPanjang (int panjang, int lebar) {
+        return panjang * lebar;
+
+                }
+        public static void main(String args[]){
+
+        }
+}
+
+
